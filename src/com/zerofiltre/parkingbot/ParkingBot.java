8819f39f860155411a24fe7d3d8b11d6769d8077
MichelleElodie <lemofouetmichelle@gmx.de>
2024-01-20 15:20:28 +0100

    p1c4 OK

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,6 +1,10 @@
 package com.zerofiltre.parkingbot;
 
+import com.zerofiltre.parkingbot.service.ParkingService;
+
 public class ParkingBot {
+  static String completion = "des equipes Zerofiltre";
+  static ParkingService parkingService = new ParkingService();
 
   /**
    * Ceci est la méthode Main
@@ -17,11 +21,17 @@ public class ParkingBot {
    * Cette méthode permet de dire Hello
    */
   private static void sayHello() {
-    String welcomeSentence = "Hello, recevez la bienvenue ";
+    String welcomeSentence = "Hello, recevez la bienvenue " +completion;
     System.out.println(welcomeSentence);
+    String parkService = parkingService.parking;
+    String washService = parkingService.washing;
+
+    System.out.println(" nous offrons les services suivant: " +parkService+ " ET " +washService);
+
+
   }
 
   private static void sayBye() {
-    System.out.println("Recevez les aurevoirs");
+    System.out.println("Recevez les aurevoirs " +completion);
   }
 }
---
+++ src/com/zerofiltre/parkingbot/service/ParkingService.java
@@ -0,0 +1,7 @@
+package com.zerofiltre.parkingbot.service;
+
+public class ParkingService {
+
+    public String parking = "Parking et surveillance de vehicule";
+    public String washing = "nettoyage de vehicules";
+}
